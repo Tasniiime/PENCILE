ecd1030cfdbf55a592e56b190bc0dc3cebe5c1d2
tasnime <tasnim.fr2008@gmail.com>
2025-01-25 21:21:15 +0100

    pincile

--- PENCILE/src/main.jsx
+++ PENCILE/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode('gray.800', '#000')(props),
+      bg: mode('gray.100', 'whiteAlpha.900')(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({config})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+  <BrowserRouter>
+  <ChakraProvider theme={theme}>
     <App />
-  </StrictMode>,
-)
+  </ChakraProvider>
+  </BrowserRouter>
+</React.StrictMode>
+);
